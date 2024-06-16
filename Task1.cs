b0bc9efd71ae8be112e5750588016fbbe0429df9
Emmanuel-tetteh <eagormeda-tetteh@st.ug.edu.gh>
2024-06-16 19:57:12 +0000

    Task 1 Commit

---
+++ Task1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some Generic Sound");
+
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("meow meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+
+        myAnimal.MakeSound();
+
+        Dog myDog = new Dog();
+
+        myDog.MakeSound();
+
+        Cat myCat = new Cat();
+
+        myCat.MakeSound();
+    }
+}
