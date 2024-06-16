c97b4f740ee4b2fc6e70e71d9ee031f27c43c6df
Emmanuel-tetteh <eagormeda-tetteh@st.ug.edu.gh>
2024-06-16 20:56:24 +0000

    Task 3 Commit

---
+++ Task3/Taks3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Task3
+{
+    static void Main()
+    {
+
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        // Call the Move method on each instance
+        car.Move();
+        bicycle.Move();
+    }
+}
